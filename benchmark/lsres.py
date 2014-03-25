9dcac30a94b667d33778fb8b114a5be34da5d05f
Francesco Romani <fromani@redhat.com>
2014-03-25 14:10:43 +0100

    more firendly interface

--- benchmark/lsres.py
+++ benchmark/lsres.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3.4
 
 import sys
-from statistics import mean, variance
+from statistics import mean, stdev
 
-def parse(src):
-    res = {}
-    for line in src:
-        items = line.strip().split('\t')
-        vm = items.pop(0)
-        res[vm] = [ float(v) for v in items ]
-    return res
+def parse(path):
+    with open(path) as src:
+        res = {}
+        for line in src:
+            items = line.strip().split('\t')
+            vm = items.pop(0)
+            res[vm] = [ float(v) for v in items ]
+        return res
 
-def show(res):
+def show(src, res):
     tot = []
     for run in zip(*res.values()):
         run = sorted(run)
-        x = (mean(run), run[0], run[-1])
+        x = (mean(run), run[0], run[-1], sum(run))
         tot.append(x)
-        print('%.3f\t%.3f\t%.3f' % x)
-    print('-' * 24)
     data = list(zip(*tot))
-    for i, x in enumerate(data):
+    desc = ('mean', 'best', 'worst', 'total')
+    print("%s:" % src)
+    for x, d in zip(data, desc):
         m = mean(x)
-        v = variance(x, m)
+        v = stdev(x)
         p = 100 * v/m
-        print('%s%.3fs +/- %.3fs (%.1f%%)' % ('\t' * i, m, v, p))
+        print('%s:\t%.3fs sd=%.3fs (%.1f%%)' % (d, m, v, p))
 
 
 if __name__ == '__main__':
-    show(parse(sys.stdin))
+    for src in sys.argv[1:]:
+        show(src, parse(src))
